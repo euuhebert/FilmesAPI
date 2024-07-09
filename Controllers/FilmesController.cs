33d9d184422df141c2c2607a8e2aa592d611bb1a
Hebert <hebertsrocha79@gmail.com>
2024-07-09 20:58:00 -0300

    add validação, add metodo get e get id, add prop id

--- Controllers/FilmesController.cs
+++ Controllers/FilmesController.cs
@@ -8,15 +8,28 @@ namespace FilmesAPI.Controllers;
 [Route("[controller]")]
 public class FilmesController : ControllerBase
 {
-    
+    private static int id = 0;
     private static List<Filme> filmes = new List<Filme>();
 
     [HttpPost]
     public void AdicionarFilme([FromBody] Filme filme)
     {
+        filme.Id = id++;
         filmes.Add(filme);
         Console.WriteLine(filme.Genero);
         Console.WriteLine(filme.Duracao);
         Console.WriteLine(filme.Titulo);
+    }
+
+    [HttpGet]
+    public List<Filme> RecuperaFilme()
+    {
+        return filmes;
+    }
+
+    [HttpGet("{id}")]
+    public Filme? RecuperaFilmeId(int id)
+    {
+      return filmes.FirstOrDefault(filme => filme.Id == id);
     }
 }
--- Models/Filmes.cs
+++ Models/Filmes.cs
@@ -1,8 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FilmesAPI.Models;
 
 public class Filme
 {
+
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Campo de Titulo não pode ser nulo")]
+    [MaxLength(500)]
     public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "Campo de  Genero não pode ser nulo")]
+    [MaxLength(50, ErrorMessage ="O Genero não pode exceder 50 carastres")]
     public string Genero { get; set; }
+
+    [Required(ErrorMessage = "Campo de duração não pode ser nulo")]
+    [Range(70, 600, ErrorMessage = "A duração deve ter entre 70 min e 10h")]
     public int Duracao { get; set; }
 }
+
+   
